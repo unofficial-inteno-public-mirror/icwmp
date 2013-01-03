1c66e25baf25deab1071fa096059687bbf694bff
zribiahmed <ahmed.zribi@pivasoftware.com>
2013-01-03 14:42:01 +0000

    <empty commit message>

--- src/scripts/freecwmp.sh
+++ src/scripts/freecwmp.sh
@@ -30,77 +30,77 @@ FLAGS "$@" || exit 1
 eval set -- "${FLAGS_ARGV}"
 
 if [ ${FLAGS_help} -eq ${FLAGS_TRUE} ]; then
-    exit 1
+	exit 1
 fi
 
 if [ ${FLAGS_newline} -eq ${FLAGS_TRUE} ]; then
-    ECHO_newline='-n'
+	ECHO_newline='-n'
 fi
 
 case "$1" in
-    set)
-        if [ "$2" = "notification" ]; then
-            __arg1="$3"
-            __arg2="$4"
-            __arg3="`echo $5| tr '[A-Z]' '[a-z]'`"
-            action="set_notification"
-        elif [ "$2" = "tag" ]; then
-            __arg1="$3"
-            __arg2="$4"
-            action="set_tag"
-        elif [ "$2" = "value" ]; then
-            __arg1="$3"
-            __arg2="$4"
-            action="set_value"
-        else
-            __arg1="$2"
-            __arg2="$3"
-            action="set_value"
-        fi
-        ;;
-    get)
-        if [ "$2" = "notification" ]; then
-            __arg1="$3"
-            action="get_notification"
-        elif [ "$2" = "tags" ]; then
-            __arg1="$3"
-            action="get_tags"
-        elif [ "$2" = "value" ]; then
-            __arg1="$3"
-            action="get_value"
-        elif [ "$2" = "name" ]; then
-            __arg1="$3"
-            __arg2=`echo $4| tr '[A-Z]' '[a-z]'`
-            action="get_name"
-        elif [ "$2" = "all" ]; then
-            __arg1="$3"
-            action="get_all"
-        else
-            __arg1="$2"
-            action="get_value"
-        fi
-        ;;
-    download)
-        action="download"
-        ;;
-    factory_reset)
-        action="factory_reset"
-        ;;
-    reboot)
-        action="reboot"
-        ;;
-    apply)
+	set)
 		if [ "$2" = "notification" ]; then
-            action="apply_notification"
-        elif [ "$2" = "value" ]; then
-            action="apply_value"
+			__arg1="$3"
+			__arg2="$4"
+			__arg3="`echo $5| tr '[A-Z]' '[a-z]'`"
+			action="set_notification"
+		elif [ "$2" = "tag" ]; then
+			__arg1="$3"
+			__arg2="$4"
+			action="set_tag"
+		elif [ "$2" = "value" ]; then
+			__arg1="$3"
+			__arg2="$4"
+			action="set_value"
+		else
+			__arg1="$2"
+			__arg2="$3"
+			action="set_value"
+		fi
+		;;
+	get)
+		if [ "$2" = "notification" ]; then
+			__arg1="$3"
+			action="get_notification"
+		elif [ "$2" = "tags" ]; then
+			__arg1="$3"
+			action="get_tags"
+		elif [ "$2" = "value" ]; then
+			__arg1="$3"
+			action="get_value"
+		elif [ "$2" = "name" ]; then
+			__arg1="$3"
+			__arg2=`echo $4| tr '[A-Z]' '[a-z]'`
+			action="get_name"
+		elif [ "$2" = "all" ]; then
+			__arg1="$3"
+			action="get_all"
+		else
+			__arg1="$2"
+			action="get_value"
+		fi
+		;;
+	download)
+		action="download"
+		;;
+	factory_reset)
+		action="factory_reset"
+		;;
+	reboot)
+		action="reboot"
+		;;
+	apply)
+		if [ "$2" = "notification" ]; then
+			action="apply_notification"
+		elif [ "$2" = "value" ]; then
+			action="apply_value"
 		else
 			action="apply_value"
 		fi
-        ;;
-    add)
+		;;
+	add)
 		if [ "$2" = "object" ]; then
-            action="add_object"
+			action="add_object"
 			__arg1="$3"
 		else
 			action="add_object"
@@ -122,31 +122,31 @@ case "$1" in
 esac
 
 if [ -z "$action" ]; then
-    echo invalid action \'$1\'
-    exit 1
+	echo invalid action \'$1\'
+	exit 1
 fi
 
 if [ ${FLAGS_debug} -eq ${FLAGS_TRUE} ]; then
-    echo "[debug] started at \"`date`\""
+	echo "[debug] started at \"`date`\""
 fi
 
 load_script() {
-    . $1 
+	. $1 
 }
 
 get_value_functions=""
 set_value_functions=""
 handle_scripts() {
-    local section="$1"
-    config_get prefix "$section" "prefix"
-    config_list_foreach "$section" 'location' load_script
-    config_get get_value_functions "$section" "get_value_function"
-    config_get get_name_functions "$section" "get_name_function"
-    config_get get_notification_functions "$section" "get_notification_function"
-    config_get set_value_functions "$section" "set_value_function"
-    config_get set_notification_functions "$section" "set_notification_function"
-    config_get add_object_functions "$section" "add_object_function"
-    config_get delete_object_functions "$section" "delete_object_function"
+	local section="$1"
+	config_get prefix "$section" "prefix"
+	config_list_foreach "$section" 'location' load_script
+	config_get get_value_functions "$section" "get_value_function"
+	config_get get_name_functions "$section" "get_name_function"
+	config_get get_notification_functions "$section" "get_notification_function"
+	config_get set_value_functions "$section" "set_value_function"
+	config_get set_notification_functions "$section" "set_notification_function"
+	config_get add_object_functions "$section" "add_object_function"
+	config_get delete_object_functions "$section" "delete_object_function"
 }
 
 config_load cwmp
@@ -176,12 +176,12 @@ FAULT_CPE_DOWNLOAD_FAIL_FILE_CORRUPTED="18"
 FAULT_CPE_DOWNLOAD_FAIL_FILE_AUTHENTICATION="19"
 
 if [ "$action" = "get_value" -o "$action" = "get_all" ]; then
-    no_fault="0"
-    freecwmp_check_fault "$__arg1"
-    fault_code="$?"
-    if [ "$fault_code" = "0" ]; then
-        if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
-            __arg1="InternetGatewayDevice."
+	no_fault="0"
+	freecwmp_check_fault "$__arg1"
+	fault_code="$?"
+	if [ "$fault_code" = "0" ]; then
+		if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
+			__arg1="InternetGatewayDevice."
 		fi
 		for function_name in $get_value_functions
 		do
@@ -193,64 +193,64 @@ if [ "$action" = "get_value" -o "$action" = "get_all" ]; then
 				if [ "$fault_code" = "$FAULT_CPE_INVALID_ARGUMENTS" ]; then
 					break
 				fi
-        done
-        if [ "$no_fault" = "1" ]; then fault_code="0"; fi
-    fi
-    if [ "$fault_code" != "0" ]; then
-        let fault_code=$fault_code+9000
-        ubus_freecwmp_output "$__arg1" "" "" "$fault_code"
-    fi
+		done
+		if [ "$no_fault" = "1" ]; then fault_code="0"; fi
+	fi
+	if [ "$fault_code" != "0" ]; then
+		let fault_code=$fault_code+9000
+		ubus_freecwmp_output "$__arg1" "" "" "$fault_code"
+	fi
 fi
 
 if [ "$action" = "get_name" -o "$action" = "get_all" ]; then
-    no_fault="0"
-    freecwmp_check_fault "$__arg1"
-    fault_code="$?"
-    if [ "$fault_code" = "0" ]; then
-        if [ \( "$__arg2" != "0" \) -a \( "$__arg2" != "1" \) -a \( "$__arg2" != "true" \) -a \( "$__arg2" != "false" \) ]; then
-            fault_code="$FAULT_CPE_INVALID_ARGUMENTS"
-        else
-            if [ "$__arg2" = "true" ]; then
-                __arg2="1"
-            elif [ "$__arg2" = "false" ]; then
-                __arg2="0"
-            fi
-        fi
-        if [ "$fault_code" = "0" ]; then
-            if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
-                ubus_freecwmp_output "InternetGatewayDevice." "0"
-                if [ \( "$__arg1" = "" \) -a \( "$__arg2" = "1" \) ]; then
-                    exit 0
-                fi
-                __arg1="InternetGatewayDevice."
-            fi
-            for function_name in $get_name_functions
-            do
-                $function_name "$__arg1" "$__arg2"
-                fault_code="$?"
-                if [ "$fault_code" = "0" ]; then
-                    no_fault="1"
-                fi
-                if [ "$fault_code" = "$FAULT_CPE_INVALID_ARGUMENTS" ]; then
-                    break
-                fi
-    done
-            if [ "$no_fault" = "1" ]; then fault_code="0"; fi
-        fi
-    fi
-    if [ "$fault_code" != "0" ]; then
-        let fault_code=$fault_code+9000
-        ubus_freecwmp_output "$__arg1" "" "" "$fault_code"
-    fi
+	no_fault="0"
+	freecwmp_check_fault "$__arg1"
+	fault_code="$?"
+	if [ "$fault_code" = "0" ]; then
+		if [ \( "$__arg2" != "0" \) -a \( "$__arg2" != "1" \) -a \( "$__arg2" != "true" \) -a \( "$__arg2" != "false" \) ]; then
+			fault_code="$FAULT_CPE_INVALID_ARGUMENTS"
+		else
+			if [ "$__arg2" = "true" ]; then
+				__arg2="1"
+			elif [ "$__arg2" = "false" ]; then
+				__arg2="0"
+			fi
+		fi
+		if [ "$fault_code" = "0" ]; then
+			if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
+				ubus_freecwmp_output "InternetGatewayDevice." "0"
+				if [ \( "$__arg1" = "" \) -a \( "$__arg2" = "1" \) ]; then
+					exit 0
+				fi
+				__arg1="InternetGatewayDevice."
+			fi
+			for function_name in $get_name_functions
+			do
+				$function_name "$__arg1" "$__arg2"
+				fault_code="$?"
+				if [ "$fault_code" = "0" ]; then
+					no_fault="1"
+				fi
+				if [ "$fault_code" = "$FAULT_CPE_INVALID_ARGUMENTS" ]; then
+					break
+				fi
+	done
+			if [ "$no_fault" = "1" ]; then fault_code="0"; fi
+		fi
+	fi
+	if [ "$fault_code" != "0" ]; then
+		let fault_code=$fault_code+9000
+		ubus_freecwmp_output "$__arg1" "" "" "$fault_code"
+	fi
 fi
 
 if [ "$action" = "set_value" ]; then
-    no_fault="0"
-    freecwmp_check_fault "$__arg1"
-    fault_code="$?"
-    if [ "$fault_code" = "0" ]; then
-        if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
-            __arg1="InternetGatewayDevice."
+	no_fault="0"
+	freecwmp_check_fault "$__arg1"
+	fault_code="$?"
+	if [ "$fault_code" = "0" ]; then
+		if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
+			__arg1="InternetGatewayDevice."
 		fi
 		for function_name in $set_value_functions
 		do
@@ -264,20 +264,20 @@ if [ "$action" = "set_value" ]; then
 			fi
 		done
 		if [ "$no_fault" = "1" ]; then fault_code="0"; fi
-    fi
-    if [ "$fault_code" != "0" ]; then
-        let fault_code=$fault_code+9000
-        freecwmp_set_parameter_fault "$__arg1" "$fault_code"
-    fi
+	fi
+	if [ "$fault_code" != "0" ]; then
+		let fault_code=$fault_code+9000
+		freecwmp_set_parameter_fault "$__arg1" "$fault_code"
+	fi
 fi
 
 if [ "$action" = "get_notification" -o "$action" = "get_all" ]; then
-    no_fault="0"
-    freecwmp_check_fault "$__arg1"
-    fault_code="$?"
-    if [ "$fault_code" = "0" ]; then
-        if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
-            __arg1="InternetGatewayDevice."
+	no_fault="0"
+	freecwmp_check_fault "$__arg1"
+	fault_code="$?"
+	if [ "$fault_code" = "0" ]; then
+		if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
+			__arg1="InternetGatewayDevice."
 		fi
 		for function_name in $get_notification_functions
 		do
@@ -291,11 +291,11 @@ if [ "$action" = "get_notification" -o "$action" = "get_all" ]; then
 			fi
 		done
 		if [ "$no_fault" = "1" ]; then fault_code="0"; fi
-    fi
-    if [ "$fault_code" != "0" ]; then
-        let fault_code=$fault_code+9000
-        ubus_freecwmp_output "$__arg1" "" "" "$fault_code"
-    fi
+	fi
+	if [ "$fault_code" != "0" ]; then
+		let fault_code=$fault_code+9000
+		ubus_freecwmp_output "$__arg1" "" "" "$fault_code"
+	fi
 fi
 
 if [ "$action" = "set_notification" ]; then
@@ -308,148 +308,148 @@ if [ "$action" = "set_notification" ]; then
 		no_fault="0"
 		freecwmp_check_fault "$__arg1"
 		fault_code="$?"
-	    if [ "$fault_code" = "0" ]; then
-	        if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
-	            __arg1="InternetGatewayDevice."
+		if [ "$fault_code" = "0" ]; then
+			if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
+				__arg1="InternetGatewayDevice."
 			fi
 			for function_name in $set_notification_functions
 			do
 				$function_name "$__arg1" "$__arg2"
-	            fault_code="$?"
-	            if [ "$fault_code" = "0" ]; then
-	                no_fault="1"
-	            fi
-	            if [ "$fault_code" = "$FAULT_CPE_INVALID_ARGUMENTS" ]; then
-	                break
-	            fi
-	        done
-	        if [ "$no_fault" = "1" ]; then fault_code="0"; fi
-	    fi
-	    if [ "$fault_code" != "0" ]; then
-	        let fault_code=$fault_code+9000
-	        freecwmp_set_parameter_fault "$__arg1" "$fault_code"
-	    fi
+				fault_code="$?"
+				if [ "$fault_code" = "0" ]; then
+					no_fault="1"
+				fi
+				if [ "$fault_code" = "$FAULT_CPE_INVALID_ARGUMENTS" ]; then
+					break
+				fi
+			done
+			if [ "$no_fault" = "1" ]; then fault_code="0"; fi
+		fi
+		if [ "$fault_code" != "0" ]; then
+			let fault_code=$fault_code+9000
+			freecwmp_set_parameter_fault "$__arg1" "$fault_code"
+		fi
 	fi
 fi
 
 if [ "$action" = "get_tags" -o "$action" = "get_all" ]; then
-    freecwmp_get_parameter_tags "x_tags" "$__arg1"
-    freecwmp_tags_output "$__arg1" "$x_tags"
+	freecwmp_get_parameter_tags "x_tags" "$__arg1"
+	freecwmp_tags_output "$__arg1" "$x_tags"
 fi
 
 if [ "$action" = "set_tag" ]; then
-    freecwmp_set_parameter_tag "$__arg1" "$__arg2"
-    /sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} commit
+	freecwmp_set_parameter_tag "$__arg1" "$__arg2"
+	/sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} commit
 fi
 
 if [ "$action" = "download" ]; then
 
-    rm /tmp/freecwmp_download 2> /dev/null
-    wget -O /tmp/freecwmp_download "${FLAGS_url}" > /dev/null 2>&1
+	rm /tmp/freecwmp_download 2> /dev/null
+	wget -O /tmp/freecwmp_download "${FLAGS_url}" > /dev/null 2>&1
 
-    dl_size=`ls -l /tmp/freecwmp_download | awk '{ print $5 }'`
-    if [ ! "$dl_size" -eq "${FLAGS_size}" ]; then
-        rm /tmp/freecwmp_download 2> /dev/null
-        exit 1
-    fi
+	dl_size=`ls -l /tmp/freecwmp_download | awk '{ print $5 }'`
+	if [ ! "$dl_size" -eq "${FLAGS_size}" ]; then
+		rm /tmp/freecwmp_download 2> /dev/null
+		exit 1
+	fi
 fi
 
 if [ "$action" = "factory_reset" ]; then
-    if [ ${FLAGS_dummy} -eq ${FLAGS_TRUE} ]; then
-        echo "# factory_reset"
-    else
-        jffs2_mark_erase "rootfs_data"
-        sync
-        reboot
-    fi
+	if [ ${FLAGS_dummy} -eq ${FLAGS_TRUE} ]; then
+		echo "# factory_reset"
+	else
+		jffs2_mark_erase "rootfs_data"
+		sync
+		reboot
+	fi
 fi
 
 if [ "$action" = "reboot" ]; then
-    /sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} set freecwmp.@local[0].event="boot"
-    /sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} commit
+	/sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} set freecwmp.@local[0].event="boot"
+	/sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} commit
 
-    if [ ${FLAGS_dummy} -eq ${FLAGS_TRUE} ]; then
-        echo "# reboot"
-    else
-        sync
-        reboot
-    fi
+	if [ ${FLAGS_dummy} -eq ${FLAGS_TRUE} ]; then
+		echo "# reboot"
+	else
+		sync
+		reboot
+	fi
 fi
 
 if [ \( "$action" = "apply_notification" \) -o \( "$action" = "apply_value" \) ]; then
-    __fault_count=`cat /var/state/cwmp 2> /dev/null |wc -l 2> /dev/null`
-    let __fault_count=$__fault_count/3
-    if [ "$__fault_count" = "0" ]; then
+	__fault_count=`cat /var/state/cwmp 2> /dev/null |wc -l 2> /dev/null`
+	let __fault_count=$__fault_count/3
+	if [ "$__fault_count" = "0" ]; then
 		# applying
-        /sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} commit
+		/sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} commit
 		if [ "$action" = "apply_value" ]; then ubus call tr069 SetParameterValuesStatus '{"status": "0"}'; fi
 		if [ "$action" = "apply_notification" ]; then ubus call tr069 SetParameterAttributes '{"success": "0", "fault_code":""}' 2> /dev/null; fi
-    else
+	else
 		let n=$__fault_count-1
-        for i in `seq 0 $n`
-        do
-            local parm=`/sbin/uci -P /var/state get cwmp.@fault[$i].parameter 2> /dev/null`
-            local fault_code=`/sbin/uci -P /var/state get cwmp.@fault[$i].fault_code 2> /dev/null`
-            ubus_freecwmp_fault_output "$parm" "$fault_code"
-            if [ "$action" = "apply_notification" ]; then break; fi
-        done
-        rm -rf /var/state/cwmp 2> /dev/null
-        /sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} revert cwmp
-    fi
+		for i in `seq 0 $n`
+		do
+			local parm=`/sbin/uci -P /var/state get cwmp.@fault[$i].parameter 2> /dev/null`
+			local fault_code=`/sbin/uci -P /var/state get cwmp.@fault[$i].fault_code 2> /dev/null`
+			ubus_freecwmp_fault_output "$parm" "$fault_code"
+			if [ "$action" = "apply_notification" ]; then break; fi
+		done
+		rm -rf /var/state/cwmp 2> /dev/null
+		/sbin/uci ${UCI_CONFIG_DIR:+-c $UCI_CONFIG_DIR} revert cwmp
+	fi
 fi
 
 if [ "$action" = "add_object" ]; then
-    no_fault="0"
-    freecwmp_check_fault "$__arg1"
-    fault_code="$?"
-    if [ "$fault_code" = "0" ]; then
-        if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
-            __arg1="InternetGatewayDevice."
+	no_fault="0"
+	freecwmp_check_fault "$__arg1"
+	fault_code="$?"
+	if [ "$fault_code" = "0" ]; then
+		if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
+			__arg1="InternetGatewayDevice."
 		fi
 		for function_name in $add_object_functions
 		do
 			$function_name "$__arg1"
-            fault_code="$?"
-            if [ "$fault_code" = "0" ]; then
-                no_fault="1"
-            fi
-            if [ "$fault_code" = "$FAULT_CPE_INVALID_ARGUMENTS" ]; then
-                break
-            fi
-        done
-        if [ "$no_fault" = "1" ]; then fault_code="0"; fi
-    fi
-    if [ "$fault_code" != "0" ]; then
-        let fault_code=$fault_code+9000
-        freecwmp_set_parameter_fault "$__arg1" "$fault_code"
-    fi
+			fault_code="$?"
+			if [ "$fault_code" = "0" ]; then
+				no_fault="1"
+			fi
+			if [ "$fault_code" = "$FAULT_CPE_INVALID_ARGUMENTS" ]; then
+				break
+			fi
+		done
+		if [ "$no_fault" = "1" ]; then fault_code="0"; fi
+	fi
+	if [ "$fault_code" != "0" ]; then
+		let fault_code=$fault_code+9000
+		ubus_freecwmp_output "$__arg1" "" "" "$fault_code"
+	fi
 fi
 
 if [ "$action" = "delete_object" ]; then
-    no_fault="0"
-    freecwmp_check_fault "$__arg1"
-    fault_code="$?"
-    if [ "$fault_code" = "0" ]; then
-        if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
-            __arg1="InternetGatewayDevice."
+	no_fault="0"
+	freecwmp_check_fault "$__arg1"
+	fault_code="$?"
+	if [ "$fault_code" = "0" ]; then
+		if [ \( "$__arg1" = "InternetGatewayDevice." \) -o \( "$__arg1" = "" \) ]; then
+			__arg1="InternetGatewayDevice."
 		fi
 		for function_name in $delete_object_functions
 		do
 			$function_name "$__arg1"
-            fault_code="$?"
-            if [ "$fault_code" = "0" ]; then
-                no_fault="1"
-            fi
-            if [ "$fault_code" = "$FAULT_CPE_INVALID_ARGUMENTS" ]; then
-                break
-            fi
-        done
-        if [ "$no_fault" = "1" ]; then fault_code="0"; fi
-    fi
-    if [ "$fault_code" != "0" ]; then
-        let fault_code=$fault_code+9000
-        freecwmp_set_parameter_fault "$__arg1" "$fault_code"
-    fi
+			fault_code="$?"
+			if [ "$fault_code" = "0" ]; then
+				no_fault="1"
+			fi
+			if [ "$fault_code" = "$FAULT_CPE_INVALID_ARGUMENTS" ]; then
+				break
+			fi
+		done
+		if [ "$no_fault" = "1" ]; then fault_code="0"; fi
+	fi
+	if [ "$fault_code" != "0" ]; then
+		let fault_code=$fault_code+9000
+		ubus_freecwmp_output "$__arg1" "" "" "$fault_code"
+	fi
 fi
 
 if [ "$action" = "inform" ]; then
@@ -463,10 +463,10 @@ if [ "$action" = "inform" ]; then
 	get_device_info_software_version
 	get_device_info_generic "InternetGatewayDevice.DeviceInfo.ProvisioningCode"
 	get_wan_device_mng_interface_ip
-    get_management_server_connection_request_url
+	get_management_server_connection_request_url
 	get_management_server_parameter_key
 fi
 
 if [ ${FLAGS_debug} -eq ${FLAGS_TRUE} ]; then
-    echo "[debug] exited at \"`date`\""
+	echo "[debug] exited at \"`date`\""
 fi
