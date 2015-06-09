be1f20251513d127b3fabe2fa3881cd4ba4aead9
Imen Bhiri <imen.bhiri@pivasoftware.com>
2015-06-09 16:20:52 +0100

    Bugg #6034: Optimization of the build of the cache - Part4: Optimisation of cwmpcache script

--- scripts/cwmpcache.sh
+++ scripts/cwmpcache.sh
@@ -7,14 +7,23 @@ local action=$2
 [ ! -f $freecwmpscript ] && exit
 [ -z $action ] && exit
 
+restart_service_maps=""
+. /usr/share/freecwmp/functions/common "RESTART_SERVICE_MAP"
+for ffile in `ls /usr/share/freecwmp/functions/`; do
+[ "$ffile" = "common"  ] && continue
+. /usr/share/freecwmp/functions/$ffile "RESTART_SERVICE_MAP"
+done
+
 sleep 2
 
 cwmp_cache() {
-	local object=$1
-	ps -w | grep freecwmp | grep $object >/dev/null || freecwmp get cache InternetGatewayDevice.$object. &
+	local object="$1"
+	local service="$2"
+	local action="$3"	
+	ps -w | grep freecwmp | grep $object >/dev/null || freecwmp get cache "$object" &
 }
 
-cwmp_voicecache() {
+cwmp_voicecache_wait_up() {
 	local max_line=""
 	while [ "$max_line" = "" -o "$max_line" = "0" ]; do
 		max_line=`/usr/sbin/asterisk -rx "brcm show status" | grep -c "Line id"`
@@ -22,99 +31,38 @@ cwmp_voicecache() {
 			sleep 1
 		fi
 	done
-	cwmp_cache "Services"
 }
 
-cwmp_firewallcache() {
-	cwmp_cache "X_INTENO_SE_IpAccCfg"
-}
-
-cwmp_icecache() {
-	cwmp_cache "X_INTENO_SE_ICE"
-}
-
-cwmp_upnpcache() {
-	cwmp_cache "UPnP"
-}
-
-cwmp_netcache() {
+cwmp_netcache_wait_up() {
 	local tm=1
 	while [ "$(nvram get wlmngr)" != "done" ]; do
 		sleep $tm
 		[ $tm -ge 10 ] && break
 		tm=$((tm+1))
 	done
-	cwmp_cache "LANInterfaces"
-	cwmp_cache "LANDevice"
-	cwmp_cache "WANDevice"
-	cwmp_cache "Layer2Bridging"
-	cwmp_cache "Layer3Forwarding"
-	cwmp_cache "UPnP"
-	cwmp_cache "X_INTENO_SE_IpAccCfg"
-	cwmp_cache "X_INTENO_SE_Wifi"
 }
 
-cwmp_passwordcache() {
-	cwmp_cache "X_INTENO_SE_LoginCfg"
+cwmp_servicecache_build() {
+	local service="$1"
+	local action="$2"
+	case $action in
+		boot|start|restart|reload)
+			for rsm in $restart_service_maps; do
+				local prefix=${rsm%%:*}
+				local rss=${rsm#*:}
+				rss=",$rss,"
+				[ "${rss/,$service,/}" = "$rss" ] && continue
+				case $service in
+					*asterisk) cwmp_voicecache_wait_up ;;
+					*network) cwmp_netcache_wait_up ;;
+				esac
+				cwmp_cache "$prefix" "$service" "$action"
+			done
+		;;
+	esac
 }
 
-cwmp_ntpcache() {
-	cwmp_cache "Time"
-}
-
-cwmp_igmpcache() {
-	cwmp_cache "X_INTENO_SE_IGMP"
-}
-
-cwmp_powermanagementcache() {
-	cwmp_cache "X_INTENO_SE_PowerManagement"
-}
-
-case $initscript in
-	*asterisk)
-		case $action in
-			boot|start|restart|reload) cwmp_voicecache ;;
-		esac
-	;;
-	*firewall)
-		case $action in
-			boot|start|restart|reload) cwmp_firewallcache ;;
-		esac
-	;;
-	*ice*)
-		case $action in
-			boot|start|restart|reload) cwmp_icecache ;;
-		esac
-	;;
-	*miniupnpd)
-		case $action in
-			boot|start|restart|reload) cwmp_upnpcache ;;
-		esac
-	;;
-	*network)
-		case $action in
-			boot|start|restart|reload) cwmp_netcache ;;
-		esac
-	;;
-	*passwords)
-		case $action in
-			boot|start|restart|reload) cwmp_passwordcache ;;
-		esac
-	;;
-	*sysntpd)
-		case $action in
-			boot|start|restart|reload) cwmp_ntpcache ;;
-		esac
-	;;
-	*mcpd)
-		case $action in
-			boot|start|restart|reload) cwmp_igmpcache ;;
-		esac
-	;;
-	*power_mgmt)
-		case $action in
-			boot|start|restart|reload) cwmp_powermanagementcache ;;
-		esac
-	;;
-esac
-
+initscript=`echo $initscript | sed -e "s/^[A-Z][0-9]*//"`
+if [ "${restart_service_maps/$initscript/}" != "$restart_service_maps" ] ;then 
+	cwmp_servicecache_build "$initscript" "$action"
+fi
