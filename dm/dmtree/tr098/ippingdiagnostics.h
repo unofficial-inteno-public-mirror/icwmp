0c137db55de32cea673bab6a41ec90815c88fb29
imen.bhiri <imen.bhiri@pivasoftware.com>
2016-05-06 14:58:16 +0100

    Ticket refs #8597 : Add IPPingDiagnostic tr098 data model parameters

---
+++ dm/dmtree/tr098/ippingdiagnostics.h
@@ -0,0 +1,16 @@
+/*
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 2 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Copyright (C) 2012-2014 PIVA SOFTWARE (www.pivasoftware.com)
+ *		Author: Imen Bhiri <imen.bhiri@pivasoftware.com>
+ */
+#ifndef __IPPING_DIAGNOSTIC_H
+#define __IPPING_DIAGNOSTIC_H
+
+extern struct ip_ping_diagnostic ipping_diagnostic; 
+int entry_method_root_IPPingDiagnostics(struct dmctx *ctx);
+
+#endif
