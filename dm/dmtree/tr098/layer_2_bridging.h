e1fed420b8ce3d2a7cf9d6aaf12c8a65c10834b9
Anis Ellouze <anis.ellouze@pivasoftware.com>
2017-04-28 09:16:33 +0100

    fix issue of Marking & code review

--- dm/dmtree/tr098/layer_2_bridging.h
+++ dm/dmtree/tr098/layer_2_bridging.h
@@ -19,4 +19,83 @@ extern DMLEAF tlayer2_bridgeParam[];
 extern DMLEAF tlayer2_markingParam[];
 extern DMLEAF tavailableinterfaceParam[];
 
+struct wan_interface 
+{
+	char *instance;
+	char *name;
+	char *package;
+	char *section;		
+};
+
+struct args_layer2
+{
+	struct uci_section *layer2section;
+	struct uci_section *layer2sectionlev2;
+	char *bridge_instance;
+	char *availableinterface_instance;
+	char *interface_type;
+	char *oface;
+};
+
+int browselayer2_availableinterfaceInst(struct dmctx *dmctx, DMNODE *parent_node, void *prev_data, char *prev_instance);
+int browselayer2_bridgeInst(struct dmctx *dmctx, DMNODE *parent_node, void *prev_data, char *prev_instance);
+int browselayer2_markingInst(struct dmctx *dmctx, DMNODE *parent_node, void *prev_data, char *prev_instance);
+int browsebridge_vlanInst(struct dmctx *dmctx, DMNODE *parent_node, void *prev_data, char *prev_instance);
+int set_marking_bridge_key_sub(char *refparam, struct dmctx *ctx, char *value);
+int set_marking_interface_key_sub(char *refparam, struct dmctx *ctx, char *value);
+int set_bridge_vlan_enable_sub(char *refparam, struct dmctx *ctx, bool b);
+int set_bridge_vlan_vid_sub(struct uci_section *vb, char *value);
+int set_bridge_vlanid_sub(char *refparam, struct dmctx *ctx, char *value);
+void remove_config_interfaces(char *baseifname, char *bridge_key, struct uci_section *bridge_s, char *mbi);
+int synchrinize_layer2_bridgeInst(struct dmctx *dmctx);
+int synchronize_availableinterfaceInst(struct dmctx *dmctx);
+inline void init_args_layer2(struct dmctx *ctx, struct uci_section *s, struct uci_section *ss,
+							char *availableinterface_instance, char *bridge_instance,
+							char *interface_type, char *oface);
+inline void init_args_layer2_vlan(struct dmctx *ctx, struct uci_section *ss);
+struct uci_section *update_availableinterface_list(struct dmctx *ctx, char *iface, char **instance, char **instance_last);
+void update_markinginterface_list(struct uci_section *interface_section, char *bridge_key, char *ifname);
+int add_layer2bridging_bridge(struct dmctx *ctx, char **instance);
+int delete_layer2bridging_bridge(struct dmctx *ctx, unsigned char del_action);
+int add_layer2bridging_marking(struct dmctx *ctx, char **instance);
+int delete_layer2bridging_marking(struct dmctx *ctx, unsigned char del_action);
+int add_layer2bridging_bridge_vlan(struct dmctx *ctx, char **instance);
+int delete_layer2bridging_bridge_vlan(struct dmctx *ctx, unsigned char del_action);
+int get_avai_int_alias(char *refparam, struct dmctx *ctx, char **value);
+int set_avai_int_alias(char *refparam, struct dmctx *ctx, int action, char *value);
+int get_marking_alias(char *refparam, struct dmctx *ctx, char **value);
+int set_marking_alias(char *refparam, struct dmctx *ctx, int action, char *value);
+int get_bridge_alias(char *refparam, struct dmctx *ctx, char **value);
+int set_bridge_alias(char *refparam, struct dmctx *ctx, int action, char *value);
+int get_bridge_vlan_enable(char *refparam,struct dmctx *ctx, char **value);
+int set_bridge_vlan_enable(char *refparam, struct dmctx *ctx, int action, char *value);
+int get_bridge_vlan_name(char *refparam, struct dmctx *ctx, char **value);
+int set_bridge_vlan_name(char *refparam, struct dmctx *ctx, int action, char *value);
+int get_bridge_vlan_vid(char *refparam, struct dmctx *ctx, char **value);
+int set_bridge_vlan_vid(char *refparam, struct dmctx *ctx, int action, char *value);
+int get_bridge_status(char *refparam, struct dmctx *ctx, char **value);
+int set_bridge_status(char *refparam, struct dmctx *ctx, int action, char *value);
+int get_bridge_key(char *refparam, struct dmctx *ctx, char **value);
+int get_bridge_name(char *refparam, struct dmctx *ctx, char **value);
+int set_bridge_name(char *refparam, struct dmctx *ctx, int action, char *value);
+int get_bridge_vlanid(char *refparam, struct dmctx *ctx, char **value);
+int set_bridge_vlanid(char *refparam, struct dmctx *ctx, int action, char *value);
+int get_associated_interfaces(char *refparam, struct dmctx *ctx, char **value);
+int set_associated_interfaces(char *refparam, struct dmctx *ctx, int action, char *value);
+int get_available_interface_key(char *refparam, struct dmctx *ctx, char **value);
+int get_interface_reference(char *refparam, struct dmctx *ctx, char **value);
+int get_interfaces_type(char *refparam, struct dmctx *ctx, char **value);
+char *layer2_get_last_section_instance(char *package, char *section, char *opt_inst);
+int update_bridge_vlan_config(char *vid, char *bridge_key);
+int update_bridge_all_vlan_config_bybridge(struct dmctx *ctx);
+void update_add_vlan_interfaces(char *bridge_key, char *vid);
+void update_add_vlan_to_bridge_interface(char *bridge_key, struct uci_section *dmmap_s);
+int get_marking_bridge_reference(char *refparam, struct dmctx *ctx, char **value);
+void get_baseifname_from_ifname(char *ifname, char *baseifname);
+int set_marking_bridge_key(char *refparam, struct dmctx *ctx, int action, char *value);
+int set_marking_interface_key(char *refparam, struct dmctx *ctx, int action, char *value);
+int set_marking_alias(char *refparam, struct dmctx *ctx, int action, char *value);
+int get_brvlan_alias(char *refparam, struct dmctx *ctx, char **value);
+int set_brvlan_alias(char *refparam, struct dmctx *ctx, int action, char *value);
+int get_marking_interface_key(char *refparam, struct dmctx *ctx, char **value);
 #endif
